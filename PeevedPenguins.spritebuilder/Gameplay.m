996b7b4358ca76b1566c9eaa1513e2e26ef08644
matthewpark319 <gracepark@Graces-MacBook-Air.local>
2014-06-25 11:50:23 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -0,0 +1,18 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Grace Park on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay {
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+
+
+@end
