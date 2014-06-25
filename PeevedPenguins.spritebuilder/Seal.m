09aecfbd5f0bcf116dbdfa0e4a09339cc075d8e9
matthewpark319 <gracepark@Graces-MacBook-Air.local>
2014-06-25 10:46:54 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,21 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Grace Park on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        NSLog(@"Seal created");
+    }
+    return self;
+}
+@end
