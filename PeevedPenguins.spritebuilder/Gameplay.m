27c1c5a74a0b569d2d7782bba9f15821539e2843
matthewpark319 <gracepark@graces-air.att.net>
2014-06-27 00:10:47 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -132,7 +132,7 @@
     float energy = [pair totalKineticEnergy];
     
     // if energy is large enough, remove the seal
-    if (energy > 1000.f) {
+    if (energy > 2000.f) {
         [[_physicsNode space] addPostStepBlock:^{
             [self sealRemoved:nodeA];
         } key:nodeA];
