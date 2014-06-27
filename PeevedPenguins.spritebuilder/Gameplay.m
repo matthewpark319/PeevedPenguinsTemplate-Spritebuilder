7bb0e6a79b0cdc262bbaece514aa7a6dc9baa8b9
matthewpark319 <gracepark@Graces-MacBook-Air.local>
2014-06-27 13:36:00 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -131,6 +131,7 @@ static const float MIN_SPEED = 5.f;
         
         // after snapping rotation is fine
         _currentPenguin.physicsBody.allowsRotation = TRUE;
+        _currentPenguin.launched = TRUE;
         
         // follow the flying penguin
         _followPenguin = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
